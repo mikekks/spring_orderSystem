13bbe14dc6fafa6ae93d7da94a5c2dd4f97bdbd5
mikekks <thdalsrb79@naver.com>
2023-08-09 16:49:25 +0900

    [Feat] 빈 조회, ApplicationContext 추가, BeanDefinitionTest

---
+++ src/test/java/hello2/core2/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,34 @@
+package hello2.core2.beanfind;
+
+import hello2.core2.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
+        }
+    }
+    @Test
+    @DisplayName("모든 빈 출력")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "  object" + bean);
+            }
+        }
+    }
+}
